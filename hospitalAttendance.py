bb77190e644d3f59442edd21d60fd8ac46c2964b
Daniel Velez <velezdaniel516@gmail.com>
2023-01-16 12:43:43 -0500

    Add problem to code scenario

---
+++ hospitalAttendance.py
@@ -0,0 +1,12 @@
+""" Scenario: Create a system that takes track of staff attendance at a local hospital when ever they sign in to work. The program should be able to:
+    1. Take employee attendance and send it to a file for database
+        * Create password for employee login
+        * keep track employee passwords in a database to check for input invalidation
+        * lock user out of system if menu options if not equal to correct choice
+    2. Mark calendar for when employees are off 
+        * Highlight days for employees with colors like red or green to tell if a employee reported to work or not
+        * Keep track of how many hours the employees worked during those days
+        * Show the name of the employee that worked that day.
+    3. Announce when there will be days off and employee bonuses.
+        * one of the menu options will display certain days on which there will be bonuses and also showing when workers can take a day off 
+"""
