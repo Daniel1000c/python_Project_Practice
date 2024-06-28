2395111afcbad67cf04caa6ad8c502d13ba59116
Daniel Velez <velezdaniel516@gmail.com>
2024-06-28 15:02:51 -0400

    add calculator.py

---
+++ pythonBeginnerPractice/calculator.py
@@ -0,0 +1,18 @@
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(f"total is {result}")
+elif operator == "-":
+    result = num1 - num2
+    print(f"total is {result}")
+elif operator == "*":
+    result = num1 * num2
+    print(f"total is {result}")
+elif operator == "/":
+    result = num1/num2
+    print(f"total is {result}")
+else:
+    print(f"{operator} is a not a valid option")
