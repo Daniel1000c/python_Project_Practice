433d1866c73716cf14ec603db99f8549c6cbeb14
Daniel Velez <velezdaniel516@gmail.com>
2024-07-15 09:31:55 -0700

    add simpleATM.py documentation

---
+++ pythonPracticeProjects/simpleATM.py
@@ -0,0 +1,26 @@
+"""
+  DATE: 7/15/2024
+  
+  TITLE: Simple ATM Program
+  
+  OBJECTIVE: Create a simple ATM program that will allow user to withdraw money from their account
+  
+  PART 1:
+  * The user starts with a balance of $1000
+  * Display following menu options to user:
+    * Note: use  a while loop for this menu option
+        1. Check Balance
+            * Just use a print statement to reveal the balance to user
+        2. Deposit Money  
+            * new balance = current balance + deposit amount
+        3. Withdraw Money
+           * newbalance = current balance - withdraw amount
+        4. Quit
+           * Exit out of while loop and end program
+
+  PART 2:
+  * Create input validation for prompt if user inputs string or letters
+  * Create an error message is user has no money in his account to withdraw money
+  * Create an error message if user deposits negative amount of money.
+    
+"""
